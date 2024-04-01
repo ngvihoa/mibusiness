4eb7c6ea5cfee97b4409c98e45369a8ce310833c
NVH <93785793+ngvihoa@users.noreply.github.com>
2024-04-01 23:43:51 +0700

    feat: run sequelize to connect db & gen model, migration, seeder

---
+++ backend/src/seeders/seeders/20240401163107-demo-user.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          email: "user2@gmail.com",
+          username: "user2",
+          password: "Hoa29072002",
+        },
+        {
+          email: "user3@gmail.com",
+          username: "user3",
+          password: "Hoa29072002",
+        },
+        {
+          email: "user4@gmail.com",
+          username: "user4",
+          password: "Hoa29072002",
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import configViewEngine from "./config/viewEngine.js";
 import initWebRoutes from "./routes/web.js";
 import bodyParser from "body-parser";
+// import connection from "./config/connectDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -18,6 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // handle request the route from browser
 initWebRoutes(app);
 
+// test connection
+connection();
 // start server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}!`);
